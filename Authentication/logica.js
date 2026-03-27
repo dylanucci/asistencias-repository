3e330f08099d4fc0222d541fb645194784a8b9aa
dylanucci <dylandanielgato@gmail.com>
2026-03-27 15:43:07 -0300

    add testing

--- Authentication/logica.js
+++ Authentication/logica.js
@@ -4,16 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const appSec = document.getElementById('app-asistencia');
     const errorMsg = document.getElementById('mensaje-error'); 
     form.addEventListener('submit', (e) => {
+
         e.preventDefault(); 
 
-        const user = document.getElementById('usuario').value;
-        const pass = document.getElementById('pass').value;
+        const user = e.target.usuario.value
+        const pass = e.target.pass.value
 
         if (user === "ADMIN" && pass === "1234") {
             errorMsg.style.display = 'none';
             loginSec.style.display = 'none';
             appSec.style.display = 'block';
             console.log("Acceso concedido");
+            window.location.href = "http://127.0.0.1:5500/interfaces/principal.html"
         } else {
             errorMsg.style.display = 'block';
             document.getElementById('pass').value = "";
